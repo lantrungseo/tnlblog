94d7cbefa27c1119f5972c92e75c98e499102959
Lan Trung Nguyen <lantrungsir28@gmail.com>
2019-05-18 18:04:58 +0700

    add route-based lazy loading

--- client/src/F_modules/index.js
+++ client/src/F_modules/index.js
@@ -1,16 +1,18 @@
-import React, {Component} from 'react';
+import React, {Component, Suspense} from 'react';
 //router
 import {Route, Switch, BrowserRouter as Router} from 'react-router-dom';
-//components
-import PrivateRoute from './shared/PrivateRoute/PrivateRoute.component'
-import Admin from './Admin/Admin.component'
-import Home from './Home/Home.component'
-import AuthRedirect from './AuthRedirect/AuthRedirect.component'
-import Post from './Post/Post.component'
 //redux
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux'
 import {fbSdkReady, checkLoginStatus} from '../B_modules/Auth/Auth.action'
+//components : lazy loading
+import PrivateRoute from './shared/PrivateRoute/PrivateRoute.component'
+import Loader from './shared/Loader/Loader.component'
+const Admin = React.lazy(()=>import('./Admin/Admin.component'))
+const Home = React.lazy(()=>import('./Home/Home.component'))
+const AuthRedirect = React.lazy(()=>import('./AuthRedirect/AuthRedirect.component'))
+const Post = React.lazy(()=>import('./Post/Post.component'))
+  
 
 class App extends Component {
   componentDidMount(){
@@ -35,16 +37,18 @@ class App extends Component {
   render(){
     return (
       <Router>
-        <Switch>
-          <PrivateRoute exact path = "/admin" component = {Admin}/>
-          <Route exact path = "/auth/redirect" component = {AuthRedirect}/>
-          <Route exact path = "/posts/:id" render = {
-            ({match})=>(
-              <Post endpoint = "verified" postID = {match.params.id}/>
-            )
-          }/>
-          <Route exact path = "/" component = {Home}/>
-        </Switch>
+        <Suspense fallback = {<Loader/>}>
+          <Switch>
+            <PrivateRoute exact path = "/admin" component = {Admin}/>
+            <Route exact path = "/auth/redirect" component = {AuthRedirect}/>
+            <Route exact path = "/posts/:id" render = {
+              ({match})=>(
+                <Post endpoint = "verified" postID = {match.params.id}/>
+              )
+            }/>
+            <Route exact path = "/" component = {Home}/>
+          </Switch>
+        </Suspense>
       </Router>
     );
   }
